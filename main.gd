8a31cbc72d38b09aab2a01afc76eeb2e9f93a387
Liam Tingley <liamtingley@gmail.com>
2025-10-26 21:48:49 -0300

    Added UI functionality + additional mob-clearing logic.

--- main.gd
+++ main.gd
@@ -9,18 +9,26 @@ var score
 @onready var start_position: Marker2D = $StartPosition
 @onready var mob_path: Path2D = $MobPath
 @onready var mob_spawn_location: PathFollow2D = $MobPath/MobSpawnLocation
+@onready var hud: CanvasLayer = $HUD
 
 func _ready():
-	new_game()
+	pass
 
 func game_over() -> void:
 	score_timer.stop()
 	mob_timer.stop()
 	
+	hud.show_game_over()
+	
 func new_game() -> void:
 	score = 0
 	player.start(start_position.position)
 	start_timer.start()
+	
+	get_tree().call_group("mobs", "queue_free")
+	
+	hud.update_score(score)
+	hud.show_message("Get Ready")
 
 
 func _on_mob_timer_timeout() -> void:
@@ -42,6 +50,7 @@ func _on_mob_timer_timeout() -> void:
 
 func _on_score_timer_timeout() -> void:
 	score += 1
+	hud.update_score(score)
 
 
 func _on_start_timer_timeout() -> void:
